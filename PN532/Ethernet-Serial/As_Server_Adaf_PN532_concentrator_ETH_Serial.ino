38478443af79bed75868c78aeae8e91262dca270
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-05-17 01:44:28 -0400

    Update As_Server_Adaf_PN532_concentrator_ETH_Serial.ino
    Implemented the use of conditional compilation (the #ifdef preprocessor directives) to select the communication method with JMRI.

--- PN532/Ethernet-Serial/As_Server_Adaf_PN532_concentrator_ETH_Serial.ino
+++ PN532/Ethernet-Serial/As_Server_Adaf_PN532_concentrator_ETH_Serial.ino
@@ -1,23 +1,30 @@
 /*
   Project: Arduino-based PN532 RFID Concentrator
   Author: Thomas Seitz (thomas.seitz@tmrci.org)
-  Version: 1.0.5
-  Date: 2023-05-16
+  Version: 1.0.6
+  Date: 2023-05-17
   Description: A sketch for an Arduino-based RFID concentrator that supports up to 8 RFID readers 
   and outputs data to Ethernet and Serial clients.
 */
+
+// Uncomment the following line to enable Ethernet communication
+// #define USE_ETHERNET
 
 // Include required libraries
 #include <SPI.h>            // SPI library for communicating with the PN532 reader
 #include <Adafruit_PN532.h> // PN532 library for reading RFID cards
 #include <string.h>         // C headers library
-#include <Ethernet.h>       // Ethernet library for the Ethernet shield
+#ifdef USE_ETHERNET
+  #include <Ethernet.h>     // Ethernet library for the Ethernet shield
+#endif
 
 // Ethernet configuration
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192, 168, 1, 177);
-unsigned int serverPort = 8888; // Replace with the desired port number
-EthernetServer server(serverPort); // Start the Ethernet server
+#ifdef USE_ETHERNET
+  byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+  IPAddress ip(192, 168, 1, 177);
+  unsigned int serverPort = 8888; // Replace with the desired port number
+  EthernetServer server(serverPort); // Start the Ethernet server
+#endif
 
 struct RFIDReader {
   char id;
@@ -42,6 +49,10 @@ RFIDReader readers[numReaders];
 
 bool isEthernetConnected = false;
 
+#ifdef USE_ETHERNET
+  EthernetClient client;  // Move this line here
+#endif
+
 void setup() {
   Serial.begin(9600);
   SPI.begin();
@@ -57,17 +68,18 @@ void setup() {
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);
 
-  if (Ethernet.begin(mac) == 1) {
-    isEthernetConnected = true;
-    delay(1000);
-  } else {
-    // Serial.println("Ethernet shield not connected or network not available");
-    isEthernetConnected = false;
-  }
+  #ifdef USE_ETHERNET
+    if (Ethernet.begin(mac) == 1) {
+      isEthernetConnected = true;
+      delay(1000);
+    } else {
+      isEthernetConnected = false;
+    }
 
-  if (isEthernetConnected) {
-    server.begin();
-  }
+    if (isEthernetConnected) {
+      server.begin();
+    }
+  #endif
 
   for (uint8_t i = 0; i < numReaders; i++) {
     readers[i].ssPin = ssPins[i];
@@ -110,11 +122,11 @@ void setup() {
 }
 
 void loop() {
-  EthernetClient client;
-
-  if (isEthernetConnected) {
-    client = server.available();
-  }
+  #ifdef USE_ETHERNET
+    if (isEthernetConnected) {
+      client = server.available();
+    }
+  #endif
 
   for (uint8_t i = 0; i < numReaders; i++) {
     if (readers[i].ssPin == 0) {
@@ -126,7 +138,6 @@ void loop() {
     uint8_t success = readers[i].pn532.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
     if (success) {
-
       memcpy(readers[i].nuid, uid, 7);
       readers[i].cardRead = true;
 
@@ -137,47 +148,33 @@ void loop() {
 
       // Send output to Serial connection
       Serial.write(readers[i].id);
-
-      // Send output to Ethernet client if connected
-      if (client) {
-        client.write(readers[i].id);
-      }
-
       for (byte j = 0; j < 5; j++) {
-        // Send output to Serial connection
         Serial.print(readers[i].nuid[j] < 0x10 ? "0" : "");
         Serial.print(readers[i].nuid[j], HEX);
-
-        // Send output to Ethernet client if connected
-        if (client) {
-          client.print(readers[i].nuid[j] < 0x10 ? "0" : "");
-          client.print(readers[i].nuid[j], HEX);
-        }
       }
-
-      // Send output to Serial connection
       Serial.print(checksum < 0x10 ? "0" : "");
       Serial.print(checksum, HEX);
-
-      // Send output to Ethernet client if connected
-      if (client) {
-        client.print(checksum < 0x10 ? "0" : "");
-        client.print(checksum, HEX);
-      }
-
-      // Send output to Serial connection
       Serial.write(0x0D);
       Serial.write(0x0A);
       Serial.write('>');
 
-      // Send output to Ethernet client if connected
-      if (client) {
-        client.write(0x0D); // CR
-        client.write(0x0A); // LF
-        client.write('>');  // ETX replaced by '>'
-      }
+      #ifdef USE_ETHERNET
+        // Send output to Ethernet client if connected
+        if (client) {
+          client.write(readers[i].id);
+          for (byte j = 0; j < 5; j++) {
+            client.print(readers[i].nuid[j] < 0x10 ? "0" : "");
+            client.print(readers[i].nuid[j], HEX);
+          }
+          client.print(checksum < 0x10 ? "0" : "");
+          client.print(checksum, HEX);
+          client.write(0x0D); // CR
+          client.write(0x0A); // LF
+          client.write('>');  // ETX replaced by '>'
+        }
+      #endif
 
-      delay(500); // Add a delay to avoid multiple reads
+      delay(500); // Add a delay to avoid multiple reads (Comment out or modify if required)
     } else {
       readers[i].cardRead = false;
     }
