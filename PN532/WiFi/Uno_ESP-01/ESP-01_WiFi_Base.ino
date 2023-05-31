ebd8dadba5676721c8a42fb8e99cef8a64474e53
The Model Railroad Club, Inc <thomas.seitz@tmrci.org>
2023-05-31 18:53:19 -0400

    Create ESP-01_WiFi_Base.ino
    Initial commit.

---
+++ PN532/WiFi/Uno_ESP-01/ESP-01_WiFi_Base.ino
@@ -0,0 +1,52 @@
+//ESP-01 WiFi Sketch for use with Arduino Uno WiFi
+ 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+ 
+// WiFi Credentials
+const char* ssid = "xxxxxx"; //Enter your SSID here
+const char* password = "xxxxx"; //Enter your password here
+const int serverPort = 8888; // JMRI port
+ 
+WiFiServer server(serverPort);
+WiFiClient client;
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println('\n');
+ 
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+ 
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  server.begin();
+}
+ 
+void loop() {
+  if (!client || !client.connected()) {
+    client = server.available();
+    if (client) {
+      Serial.println("");
+    }
+  }
+ 
+  if (client && client.connected())
+    if (Serial.available()) {
+      while (Serial.available()) {
+        char data = Serial.read();
+        client.write(data);
+      }
+    }
+  }
